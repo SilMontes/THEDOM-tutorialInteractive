dfb7e5fb8eb74b446471d836ad10e0aabad0870d
SilMontes <Silmontes2001@gmail.com>
2021-02-28 13:55:59 +0000

    Avance

--- exercises/13-Add-Options-to-the-Select/index.js
+++ exercises/13-Add-Options-to-the-Select/index.js
@@ -3,5 +3,15 @@ window.onload = function() {
 	let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
 
 	// your code here}
-	
+
+	let mySelect = document.getElementById("mySelect");
+	let selectValue = mySelect.value;
+	let pais = 0;
+	for (var i = 0; i < countries.length; i++) {
+		pais = countries[i];
+		mySelect.appendChild(pais);
+	}
+	mySelect.addEventListener("change", () => {
+		alert(event.target.value);
+	});
 };
