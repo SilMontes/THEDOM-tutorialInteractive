d9bc253723d4c898f60fe2cfbd716b222cbb01e7
SilMontes <Silmontes2001@gmail.com>
2021-03-01 21:28:12 +0000

    Almost Done

--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -1,23 +1,11 @@
-// Your code here
-// let input = document.querySelector("#addTodo").value;
 
-// let basurero = document.getElementById("myList");
-// for (let i = 0; i < basurero.length; i++) {
-// 	basurero[i].onclick = function() {
-// 		let myUl = document.querySelector("#myList");
-// 		let hijos = myUl.childNodes;
-// 		hijos.parentNode.removeChild(e.target);
-// 	};
-// }
-// let documento = document.getElementById("container");
-// documento.addEventListener("blur", agregar);
 
 let button = document.querySelector("#addToDo");
 button.addEventListener("click", function() {
 	//your code here
 	document.querySelector("#myList").childNodes;
 	let elem = document.createElement("li");
-	let myString = "<span><i class='fa fa-trash'></i> Shower</span>";
+	let myString = "<span><i class='fa fa-trash'></i>Shower</span>";
 	elem.innerHTML = myString;
 	document.getElementById("myList").appendChild(elem);
 });
