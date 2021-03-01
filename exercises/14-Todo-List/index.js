5456a857c87c49d22f070186988cc9ebcc6741aa
SilMontes <Silmontes2001@gmail.com>
2021-03-01 19:36:46 +0000

    One missing

--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -1,16 +1,42 @@
 // Your code here
-let input = document.querySelector("#addTodo").value;
+// let input = document.querySelector("#addTodo").value;
 
-let button = document.createElement("button");
-button.type = "button";
-button.innerText = "Add Task";
-document.getElementById("container").appendChild(button);
+// let basurero = document.getElementById("myList");
+// for (let i = 0; i < basurero.length; i++) {
+// 	basurero[i].onclick = function() {
+// 		let myUl = document.querySelector("#myList");
+// 		let hijos = myUl.childNodes;
+// 		hijos.parentNode.removeChild(e.target);
+// 	};
+// }
+// let documento = document.getElementById("container");
+// documento.addEventListener("blur", agregar);
 
-let basurero = document.getElementsByClassName("fa fa-trash");
-for (let i = 0; i < basurero.length; i++) {
-	basurero.onclick = function() {
-		let myUl = document.querySelector("ul");
-		let children = myUl.childNodes;
-		myUl.choldre.removeChild();
+let button = document.querySelector("#addToDo");
+button.addEventListener("click", function() {
+	//your code here
+	document.querySelector("#myList").childNodes;
+	let elem = document.createElement("li");
+	let myString = "<span><i class='fa fa-trash'></i> Shower</span>";
+	elem.innerHTML = myString;
+	document.getElementById("myList").appendChild(elem);
+});
+
+let items = document.querySelectorAll("#myList li"),
+	tab = [],
+	liIdx;
+
+for (let i = 0; i < items.length; i++) {
+	tab.push(items[i].innerHTML);
+}
+
+for (let i = 0; i < items.length; i++) {
+	items[i].onclick = function() {
+		liIdx = tab.indexOf(this.innerHTML);
+		console.log(this.innerHTML + " INDEX = " + liIdx);
+		let ul = document.querySelector("#myList");
+		console.log(liIdx);
+		let liToDelete = ul.childNodes[liIdx];
+		liToDelete.parentNode.removeChild(liToDelete);
 	};
 }
